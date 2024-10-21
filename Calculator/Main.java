77436c032fdaa0ecfa597fddcbee4b5d7d4ab3bb
SahithiMekala <mekala.sahithi@carrier.com>
2024-10-21 20:11:32 +0530

    comment input

--- Calculator/Main.java
+++ Calculator/Main.java
@@ -6,6 +6,7 @@ public class Main{
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter two nums:");
+        //taking two numbers as input
         int a = Integer.parseInt(reader.nextLine());
         int b = Integer.parseInt(reader.nextLine());
         System.out.println("Enter operation you want to perform:");
